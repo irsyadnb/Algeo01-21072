20761f1dcbdf0d6c6e221c9405ed0c426cc21e39
auliamey <89340189+auliamey@users.noreply.github.com>
2022-09-30 12:27:38 +0700

    add cramer

--- src/SPL.java
+++ src/SPL.java
@@ -4,12 +4,14 @@ public class SPL {
     static Scanner in = new Scanner(System.in); // Input String
     static Scanner sc = new Scanner(System.in); // Input integer/double
     // Eliminasi Gauss
+
     public static Matrix ubahEselon(Matrix m){
-        int i, j, a, aa, c;
+        int i, j, a, aa, c, carinol;
         boolean keep;
         double bagi, nilai, k;
         Matrix mHasil = new Matrix(m.getRow(), m.getColumn());
         m.copyMatrix(mHasil);
+        mHasil.displayMatrix();
 
         for (i=0; i<mHasil.getRow(); i++){
             if (mHasil.getELMT(i, i) != 1 || mHasil.getELMT(i, i) == 0){
@@ -21,9 +23,9 @@ public class SPL {
                         keep = false;
                     }
                     c++;
-                }
+                }   
+                mHasil.displayMatrix();
             }
-
             
             // bagi baris  agar yang pertama jadi 1
             bagi = 1 / mHasil.getELMT(i, i);
@@ -31,19 +33,27 @@ public class SPL {
             for (j=i; j<mHasil.getColumn(); j++){
                 mHasil.setELMT(i, j, (mHasil.getELMT(i, j)*bagi));
             }
+            mHasil.displayMatrix();
 
             //membuat elemen di bawah 1 utama menjadi 0
             for(a=i+1; a<mHasil.getRow(); a++){
                 k = mHasil.getELMT(a, i) / mHasil.getELMT(i, i);
+                carinol = 0;
                 for (aa = i; aa < mHasil.getColumn(); aa++){
                     nilai = mHasil.getELMT(a, aa) - k * mHasil.getELMT(i, aa);
+                    if (nilai == 0){
+                        carinol++;
+                    }
                     mHasil.setELMT(a, aa, nilai);
+                }
+                if (carinol == 2){
+                    break;
                 }
             }
             
         }
+        mHasil.displayMatrix();
         return mHasil;
-        
     }
 
     // Eliminasi Gauss-Jordan
@@ -53,17 +63,16 @@ public class SPL {
         // iterasi keatas, dari sudut kanan bawah 
         for (int i = m.getRow()-1 ; i >= 0; i--){ 
             for(int j = m.getColumn()-1 ; j >= 0; j--){
-
-                if (m.getELMT(i, j)==1){
+                if (m.getELMT(i, j) == 1){
                     double factor;
-                    int otherRow = i-1;
+                    int otherRow = i -1;
+                    m.displayMatrix();
 
                     while (otherRow >= 0){
                         factor = m.getELMT(otherRow, j);
-
                         m.otherKRow(otherRow, i, factor);
-
                         otherRow--;
+                        m.displayMatrix();
                     }
                 }
             }
@@ -71,4 +80,23 @@ public class SPL {
         return m;
     }
 
+    public static Matrix Cramer(Matrix m){
+        int i,j;
+        double detkoef, detCramer;
+        Matrix koef, kons, cramer;
+        koef = Matrix.coeffMat(m);
+        kons = Matrix.constMat(m);
+        cramer = new Matrix(kons.getRow(), 1);
+        detkoef = Determinan.detKofaktor(koef);
+
+        for (j=0; j<koef.getColumn(); j++){
+            koef = Matrix.coeffMat(m);
+            for (i=0; i<koef.getRow(); i++){
+                koef.setELMT(i, j, kons.getELMT(i, 0));
+            }
+            detCramer = Determinan.detKofaktor(koef);
+            cramer.setELMT(j, 0, detCramer/detkoef);
+        }
+        return cramer;
+    }
 }

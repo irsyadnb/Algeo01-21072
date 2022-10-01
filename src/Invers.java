0734e0fbe7e63bf50884056ef0f90ea8c0b32e5b
auliamey <89340189+auliamey@users.noreply.github.com>
2022-10-02 02:35:04 +0700

    update inversgauss

--- src/Invers.java
+++ src/Invers.java
@@ -1,44 +1,77 @@
 public class Invers {
   //Mencari inv matriks dengan Gauss-Jordan
-   public static Matrix gaussInv(Matrix m) {
-    int i, j, k;
-    Matrix matKiri = new Matrix(m.getRow(), m.getColumn());
-    Matrix matKanan = new Matrix(m.getRow(), m.getColumn());
-    m.copyMatrix(matKiri);
-    m.copyMatrix(matKanan);
-    matKanan.identmMatrix(matKanan);;
-    for (i = 0; i < matKiri.getRow(); i++) {
-      if (matKiri.getELMT(i, i) == 0) { //Menukar kalo ada elmt diagonal yg 0
-        boolean flag = true;
-        k = i + 1;
-        while (k < matKiri.getRow() && flag) {
-          if (matKiri.getELMT(k, i) != 0) {
-            matKiri.swapRow(i, k);
-            matKanan.swapRow(i, k);
-            flag = false;
-          }
+   //Mencari inv matriks dengan Gauss-Jordan
+     public static Matrix InversGaussJordan(Matrix m) {
+        int i, j, a, aa, c, x, y;
+        boolean keep;
+        double bagi, k, nilai, nilai2;
+        // buat dua matriks kanan kiri untuk dilakukan gauss-jordan
+        Matrix m1 = new Matrix (m.getRow(), m.getColumn());
+        Matrix m2 = new Matrix (m.getRow(), m.getColumn());
+        m.copyMatrix(m1);
+        m.copyMatrix(m2);
+
+        m2.MatrixIdentitas();
+
+        for (i=0; i<m1.getRow(); i++){
+            if (m1.getELMT(i, i) != 1 || m1.getELMT(i, i) == 0){
+                c  = i + 1;
+                keep = true;
+                while (c < m1.getRow() && keep){
+                    if (m1.getELMT(c, i) == 1){
+                        m1.swapRow(i, c);
+                        m2.swapRow(i, c);
+                        keep = false;
+                    }
+                    c++;
+                }   
+            }
+
+            // bagi baris  agar yang pertama jadi 1
+            if (m1.getELMT(i, i) != 0){
+                bagi = 1 / m1.getELMT(i, i);
+            } else{
+                break;
+            }
+
+            for (j=i; j<m1.getColumn(); j++){
+                m1.setELMT(i, j, (m1.getELMT(i, j)*bagi));
+            }
+
+            for (y=0; y<m2.getColumn(); y++){
+                m2.setELMT(i, y, (m2.getELMT(i, y)*bagi));
+            }
+
+            //membuat elemen di bawah 1 utama menjadi 0
+            for(a=i+1; a<m1.getRow(); a++){
+                k = m1.getELMT(a, i) / m1.getELMT(i, i);
+                for (aa = i; aa < m1.getColumn(); aa++){
+                    nilai = m1.getELMT(a, aa) - k * m1.getELMT(i, aa);
+                    m1.setELMT(a, aa, nilai);
+                }
+                for (x=0; x<m2.getColumn(); x++){
+                    nilai2 = m2.getELMT(a, x) - k * m2.getELMT(i, x);
+                    m2.setELMT(a, x, nilai2);
+                }
+            }
         }
-      }
-      if (matKiri.getELMT(i, i) != 1) { //Bagi koef
-        matKiri.divRow(i, matKiri.getELMT(i, i));
-        matKanan.divRow(i, matKiri.getELMT(i, i));
-      }
-      for (j = 0; j < matKiri.getRow(); j++) { //OBE di pivot
-        if (matKiri.getELMT(j, i) != 0 && i != j) {
-          matKiri.otherKRow(j, i, matKiri.getELMT(j, i));
-          matKanan.otherKRow(j, i, matKiri.getELMT(j, i));
+        for (i = m1.getRow()-1 ; i >= 0; i--){ 
+            for(j = m1.getColumn()-1 ; j >= 0; j--){
+                if (m1.getELMT(i, j) == 1){
+                    double factor;
+                    int otherRow = i -1;
+
+                    while (otherRow >= 0){
+                        factor = m1.getELMT(otherRow, j);
+                        m1.otherKRow(otherRow, i, factor);
+                        m2.otherKRow(otherRow, i, factor);
+                        otherRow--;
+                    }
+                }
+            }
         }
-      }
-    }
-    for (i = 0; i < matKanan.getRow(); i++) {
-      for (j = 0; j < matKanan.getColumn(); j++) {
-        if (matKanan.m[i][j] == -0.0) {
-          matKanan.m[i][j] = Math.abs(matKanan.m[i][j]);
-        }
-      }
-    }
-    return matKanan;
-  }
+        return m2;
+     }
     
   // Mencari kofaktor matriks
     public static Matrix getKofMatrix (Matrix m, int pivotrow, int pivotcol) {

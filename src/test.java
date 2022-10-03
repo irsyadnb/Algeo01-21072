a3319cd4c1cb239ae100215a739c2d94ec1c8855
Irsyad Nurwidianto Basuki <56458931+irsyadnb@users.noreply.github.com>
2022-10-03 09:36:16 +0700

    Delete test.java

--- src/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        BacaFile file = new BacaFile("/Users/irsyadnb/Downloads/ITB Jaya/SMT 3/Tubes/Algeo/tubes1/test.txt");
-        BacaFile.writeMatrix(file.bacaFile(), "/Users/irsyadnb/Downloads/ITB Jaya/SMT 3/Tubes/Algeo/tubes1/test.txt" );
-
-    }   
-
-}
